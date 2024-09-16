fdc9be49c65d251e01d81c86f48c2f03e276151c
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2024-09-16 18:14:59 +0900

    Quartz sync: Sep 16, 2024, 6:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Courgette",
+        header: "Rufina",
         body: "Frank Ruhl Libre",
         code: "IBM Plex Mono",
       },
