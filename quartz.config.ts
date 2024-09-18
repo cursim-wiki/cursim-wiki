c8a7baed0c1c0afbb64b1f288bcea6457ec432ce
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2024-09-18 23:22:06 +0900

    Quartz sync: Sep 18, 2024, 11:22 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "cursim-wiki.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "published",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
