76f7ece2c54a9f2484996d52bda61458298f8d28
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2024-10-10 23:22:53 +0900

    Quartz sync: Oct 10, 2024, 11:22 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,6 +33,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
+  afterBody: [Component.MobileOnly(Component.Explorer())],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
@@ -46,4 +47,5 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
+  afterBody: [],
 }
