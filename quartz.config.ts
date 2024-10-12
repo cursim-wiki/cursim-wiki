a52de7479f6d425c0f23f6e6e195d031deb5f477
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2024-10-12 13:01:51 +0900

    Quartz sync: Oct 12, 2024, 1:01 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,7 +12,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-JNQ90SHZKG",
     },
     locale: "en-US",
     baseUrl: "cursim-wiki.github.io",
